694a8f97482f676b803bdf303de2f46c99c6f9a9
Ilya Molokov <lumberjack.im@gmail.com>
2023-08-18 16:52:12 +0600

    fix: инициалы если нет аватарки

--- src/widgets/Header/components/OnlineUser/OnlineUser.tsx
+++ src/widgets/Header/components/OnlineUser/OnlineUser.tsx
@@ -12,15 +12,20 @@ interface HeaderUserProps {
   yandexUser: IYandexUser
 }
 
-export const OnlineUser: FC<HeaderUserProps> = ({ yandexUser }) => {
+export const OnlineUser: FC<HeaderUserProps> = ({ yandexUser}) => {
   const avatarUrl = getAvatarUrl(yandexUser.default_avatar_id)
+  const userInitials = 
+    yandexUser.default_avatar_id === "0/0-0" 
+    ? `${yandexUser.first_name[0]}. ${yandexUser.last_name[0]}.`
+    : ""
 
+  console.log(yandexUser)
   return (
     <div className={styles.user}>
       <div className={styles.avatar}>
         <img className={styles.avatarImg} src={avatarUrl} alt="avatar" />
+        {userInitials ?? <span>{userInitials}</span>}
       </div>
-
       <ControlStatusContainer id={yandexUser.id} />
     </div>
   )
