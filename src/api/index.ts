61ac42a4c95cac8f1c2a7fc8114283fe1184408b
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-08-18 18:10:51 +0600

    fix: fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
+// возврат api
+
 import { urls } from '@constants/urls'
 import { checkAuthorizationToken } from '@helpers/checkAuthorizationToken'
 import { configInterceptor } from '@helpers/configInterceptor'
