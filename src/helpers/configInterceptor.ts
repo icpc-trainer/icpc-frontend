50621fdd259b8ea42228d4e570486863da6c8565
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-08-19 17:45:52 +0600

    fix: запрос комментариев к задаче

--- src/helpers/configInterceptor.ts
+++ src/helpers/configInterceptor.ts
@@ -6,19 +6,23 @@ import { getCookie } from './getCookie'
 import { setCookie } from './setCookie'
 
 export const configInterceptor = (config: InternalAxiosRequestConfig) => {
+  const isMessagesUrl = /training-sessions\/.*\/problem\/.*\/comments/.test(config.url)
+
   const urlParams = new URLSearchParams(window.location.hash)
   const accessToken = urlParams.get('#access_token')
 
   if (accessToken) {
     const expiresIn = urlParams.get('expires_in')
     setCookie('access_token', accessToken, expiresIn)
-    config.headers['Authorization'] = `OAuth ${accessToken}`
+    if (!isMessagesUrl) {
+      config.headers['Authorization'] = `OAuth ${accessToken}`
+    }
   } else {
     if (!document.cookie || !document.cookie.includes('access_token')) {
       window.location.replace(urls.yandexPassport)
     } else {
       const token = getCookie('access_token')
-      if (token) {
+      if (token && !isMessagesUrl) {
         config.headers['Authorization'] = `OAuth ${token}`
       }
     }
