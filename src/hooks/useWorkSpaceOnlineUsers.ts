71623428890f6695fc4016282e95f5521a409c04
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-08-19 18:08:13 +0600

    fix: хуки онлайн пользователей

--- src/hooks/useWorkSpaceOnlineUsers.ts
+++ src/hooks/useWorkSpaceOnlineUsers.ts
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router'
 
+import { workSpaceSocket } from '@sockets/work-space-socket'
+
 import { api } from '@api/index'
 
 import { useOnlineUsers } from '@hooks/useOnlineUsers'
@@ -8,10 +10,18 @@ import { useOnlineUsers } from '@hooks/useOnlineUsers'
 export const useWorkSpaceOnlineUsers = () => {
   const { trainingSessionId } = useParams()
 
-  const { users, setUsers } = useOnlineUsers()
+  const { users, setUsers, userEventHandler, userLeaveEventHandler } = useOnlineUsers()
 
   useEffect(() => {
     api.getWorkSpaceOnlineUsers(trainingSessionId).then(setUsers).catch(console.log)
+
+    const userUnsubscribe = workSpaceSocket.subscribeUser(userEventHandler)
+    const userLeaveUnsubscribe = workSpaceSocket.subscribeUserLeave(userLeaveEventHandler)
+
+    return () => {
+      userUnsubscribe()
+      userLeaveUnsubscribe()
+    }
   }, [])
 
   return { users }
