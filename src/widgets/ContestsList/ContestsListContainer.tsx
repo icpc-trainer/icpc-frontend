6c093ae5370b7bb4c6e23c6d88e649820ec8f6a4
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-08-23 23:28:23 +0600

    fix: отфильтровать pdf контесты

--- src/widgets/ContestsList/ContestsListContainer.tsx
+++ src/widgets/ContestsList/ContestsListContainer.tsx
@@ -1,13 +1,14 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 
 import { useGetContestsQuery } from '@store/api/api'
 
 import { ContestsList } from './ContestsList'
+import { pdfContests } from "@constants/pdfContests"
 
 export const ContestsListContainer: FC = () => {
   const { data: contests } = useGetContestsQuery()
 
   if (!contests) return null
 
-  return <ContestsList contests={contests} />
+  return <ContestsList contests={contests.filter(({ id }) => !pdfContests.includes(Number(id)))} />
 }
